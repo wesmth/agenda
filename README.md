5aba790e830dc7ab9538b7a530526503852ad87d
Weslen Matheus <wesmth@gmail.com>
2025-06-26 21:03:05 -0300

    Criada a primeira model para a criação dos contatos, e posteriormente migradas

--- README.md
+++ README.md
@@ -36,3 +36,23 @@ python manage.py migrate
 python manage.py createsuperuser
 python manage.py changepassword USERNAME
 ```
+
+# Criando as models
+
+```
+# No arquivo models.py do app contact, criamos a seguinte model
+
+class Contact(models.Model):
+    # para registrar uma pessoa, todos os campos abaixo são obrigatórios
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=50, blank=True)#colocando o blank, tiramos a obrigatoriedade do campo
+    # a partir daqui não é mais obrigatório
+    created_date = models.DateTimeField(default=timezone.now) #colocamos o timezone.now para que o django saiba que data o contato foi criado e assim o usuario não consegue editar esse campo manualmente.
+    description = models.TextField(blank=True) #apesar de tanto o charfield quanto o textfield serem campos de texto/string, o charfield tem limitação de caracteres ja o textfield não.
+
+    # Depois de criada, rodamos no terminal:
+    python manage.py makemigrations # Cria as migrações
+    python manage.py migrate # Migra
+``` 
--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.utils import timezone # importamos isso para sabermos a data atual
 
-# Create your models here.
+# id(primary key - automatico o proprio django cria)
+# first_name (string) last_name(string)
+# email (email), created_date(date) description (text)
+# category (foreign key) show (boolean) owner (foreign key)
+# picture (imagem)
+
+class Contact(models.Model):
+    # para registrar uma pessoa, todos os campos abaixo são obrigatórios
+    first_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    phone = models.CharField(max_length=10)
+    email = models.EmailField(max_length=50, blank=True)#colocando o blank, tiramos a obrigatoriedade do campo
+    # a partir daqui não é mais obrigatório
+    created_date = models.DateTimeField(default=timezone.now) #colocamos o timezone.now para que o django saiba que data o contato foi criado e assim o usuario não consegue editar esse campo manualmente.
+    description = models.TextField(blank=True) #apesar de tanto o charfield quanto o textfield serem campos de texto/string, o charfield tem limitação de caracteres ja o textfield não.
+
+
+    
