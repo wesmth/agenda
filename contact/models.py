7535a2f847eb7f05302e65941d5df7e78b017745
Weslen Matheus <wesmth@gmail.com>
2025-06-27 20:54:35 -0300

    A model foi adicionada na parte administrativa do django e os primeiros contatos foram criados.

--- contact/models.py
+++ contact/models.py
@@ -11,11 +11,13 @@ class Contact(models.Model):
     # para registrar uma pessoa, todos os campos abaixo são obrigatórios
     first_name = models.CharField(max_length=35)
     last_name = models.CharField(max_length=35)
-    phone = models.CharField(max_length=10)
+    phone = models.CharField(max_length=11)
     email = models.EmailField(max_length=50, blank=True)#colocando o blank, tiramos a obrigatoriedade do campo
     # a partir daqui não é mais obrigatório
     created_date = models.DateTimeField(default=timezone.now) #colocamos o timezone.now para que o django saiba que data o contato foi criado e assim o usuario não consegue editar esse campo manualmente.
     description = models.TextField(blank=True) #apesar de tanto o charfield quanto o textfield serem campos de texto/string, o charfield tem limitação de caracteres ja o textfield não.
 
 
-    
+    # esta parte abaixo faz com que ao ivnez de retornar algo estranho, retorne o nome completo do contato.
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
